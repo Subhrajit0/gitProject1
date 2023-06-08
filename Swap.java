c1417a6cb5c9143fe08285c108c7cfa5f512ffb2
Subhrajit <subhrajitmaharana*@gmail.com>
2023-06-08 23:52:46 +0530

    Swap elements.

---
+++ Swap.java
@@ -0,0 +1,19 @@
+public class Swap {
+
+	public static void main(String args[]) {
+
+		int a = 25 ;
+		int b = 10 ;
+		
+		int temp ;
+
+		temp = a ;
+		a = b ;
+		b = temp ;
+
+		System.out.println("Value of a is : " +a+ "\nValue of b is : " +b) ;
+	
+
+	}
+
+}
